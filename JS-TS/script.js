c053e4ce6c04f5bde1fba4fd6807faec61435515
Vilas Chavhan <vilas.chavhan@seaflux.tech>
2023-06-22 11:56:01 +0530

    feature: added Update functionality

--- JS-TS/script.js
+++ JS-TS/script.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,112 +8,61 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var _this = this;
 //getting form and form values
-var AddForm = document.querySelector('.form');
-var id = document.querySelector('#id');
-var RecipeName = document.querySelector("#name");
-var type = document.querySelector("#type");
-var desc = document.querySelector("#desc");
-var ul = document.querySelector('.ul');
-//Description modal 
-var heading = document.querySelector('#RecipeName');
-var description = document.querySelector("#recipeDescription");
+const AddForm = document.querySelector('.form');
+const RecipeName = document.querySelector("#name");
+const type = document.querySelector("#type");
+const desc = document.querySelector("#desc");
+const ul = document.querySelector('.ul');
+//Recipe View modal 
+const heading = document.querySelector('#RecipeName');
+const description = document.querySelector("#recipeDescription");
+const Recipetype = document.querySelector("#recipeType");
+//Delete modal
+const DeleteName = document.querySelector("#DeleteName");
+const DeleteConfirm = document.querySelector("#DeleteConfirm");
+const DeleteBtn = document.querySelector("#Delete");
+//update form value:
+const Uform = document.querySelector('.update-form');
+const Uname = document.querySelector("#Uname");
+const Utype = document.querySelector("#Utype");
+const Udesc = document.querySelector("#Udesc");
+let Uid;
 //function to fetch the data;
-var getData = function () { return __awaiter(_this, void 0, void 0, function () {
-    var res, data, err_1;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 3, , 4]);
-                return [4 /*yield*/, fetch('http://localhost:3000/recipes')];
-            case 1:
-                res = _a.sent();
-                return [4 /*yield*/, res.json()];
-            case 2:
-                data = _a.sent();
-                data.map(function (item) {
-                    console.log(item);
-                    RenderList(item);
-                    // view(item.id);
-                });
-                return [3 /*break*/, 4];
-            case 3:
-                err_1 = _a.sent();
-                console.log(err_1);
-                return [3 /*break*/, 4];
-            case 4: return [2 /*return*/];
-        }
-    });
-}); };
+const getData = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const res = yield fetch('http://localhost:3000/recipes');
+        const data = yield res.json();
+        data.map((item) => {
+            RenderList(item);
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
 getData();
 //function to render the Recipe lists
-var RenderList = function (listData) {
-    var li = document.createElement('li');
+const RenderList = (listData) => {
+    const li = document.createElement('li');
     li.classList.add("bg-light", "d-flex", "justify-content-between", "p-2");
     //This will create the span tag which will show name of Recipe
-    var spanLeft = document.createElement('span');
+    const spanLeft = document.createElement('span');
     spanLeft.innerText = listData.name;
     //this will create the right side span which will have the view update delete button;
-    var spanRight = document.createElement('span');
-    spanRight.innerHTML = "<i onclick=\"viewRecipe(" + listData.id + ")\" data-bs-target=\"#descriptionModal\" data-bs-toggle=\"modal\" class=\"bi bi-eye btn btn-primary\"></i> \n    <i class=\"bi bi-pencil btn btn-warning\"></i> \n    <i onclick=\"Delete(" + listData.id + ")\" class=\"bi bi-trash3 btn btn-danger\"></i>";
+    const spanRight = document.createElement('span');
+    spanRight.innerHTML = `
+    <i onclick="viewRecipe(${listData.id})" data-bs-target="#descriptionModal" data-bs-toggle="modal" class="bi bi-eye btn btn-primary"></i> 
+    <i onclick="EditModal('${listData.id}')" data-bs-target="#UpdateModal" data-bs-toggle="modal" class="bi bi-pencil btn btn-warning"></i> 
+    <i data-bs-target="#DeleteModal" data-bs-toggle="modal" onclick="Delete('${listData.name}', ${listData.id})" class="bi bi-trash3 btn btn-danger"></i>`;
     li.append(spanLeft, spanRight);
     ul.append(li);
 };
-//this function will trigger view modal to view the recipe info:
-var viewRecipe = function (id) { return __awaiter(_this, void 0, void 0, function () {
-    var res, recipeData, _a, _b;
-    return __generator(this, function (_c) {
-        switch (_c.label) {
-            case 0: return [4 /*yield*/, fetch("http://localhost:3000/recipes/" + id)];
-            case 1:
-                res = _c.sent();
-                return [4 /*yield*/, res.json()];
-            case 2:
-                recipeData = _c.sent();
-                _a = heading;
-                return [4 /*yield*/, recipeData.name];
-            case 3:
-                _a.innerText = _c.sent();
-                _b = description;
-                return [4 /*yield*/, recipeData.desc];
-            case 4:
-                _b.innerText = _c.sent();
-                return [2 /*return*/];
-        }
-    });
-}); };
-AddForm.addEventListener('submit', function (e) {
+//submit data on form submit
+AddForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    var Recipe = {
-        id: id.valueAsNumber,
+    const Recipe = {
+        id: Date.now(),
         name: RecipeName.value,
         type: type.value,
         desc: desc.value
@@ -120,52 +70,82 @@ AddForm.addEventListener('submit', function (e) {
     postData(Recipe);
 });
 //This function posts data into database
-var postData = function (data) { return __awaiter(_this, void 0, void 0, function () {
-    var res, result, err_2;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 3, , 4]);
-                return [4 /*yield*/, fetch('http://localhost:3000/recipes', {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify(data)
-                    })];
-            case 1:
-                res = _a.sent();
-                return [4 /*yield*/, res.json()];
-            case 2:
-                result = _a.sent();
-                console.log("data Uploaded", result);
-                return [3 /*break*/, 4];
-            case 3:
-                err_2 = _a.sent();
-                console.log("ERROR: ", err_2);
-                return [3 /*break*/, 4];
-            case 4: return [2 /*return*/];
-        }
-    });
-}); };
-//function to send delete request data from database:
-var DeleteRequest = function (id) { return __awaiter(_this, void 0, void 0, function () {
-    return __generator(this, function (_a) {
-        fetch("http://localhost:3000/recipes/" + id, {
-            method: "DELETE"
+const postData = (data) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const res = yield fetch('http://localhost:3000/recipes', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
         });
-        console.log("data deleted successfully....");
-        return [2 /*return*/];
+        const result = yield res.json();
+        console.log("data Uploaded", result);
+    }
+    catch (err) {
+        console.log("ERROR: ", err);
+    }
+});
+//this function will trigger view modal to view the recipe info:
+const viewRecipe = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield fetch(`http://localhost:3000/recipes/${id}`);
+    const recipeData = yield res.json();
+    heading.innerText = yield recipeData.name;
+    description.innerText = yield recipeData.desc;
+    Recipetype.innerText = yield recipeData.type;
+});
+//function to send delete request data from database:
+const DeleteRequest = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    fetch(`http://localhost:3000/recipes/${id}`, {
+        method: "DELETE",
     });
-}); };
+    console.log("data deleted successfully....");
+});
 //function to delete the item from database;
-var Delete = function (id) {
-    var confirm = window.confirm("Are you sure you want to delete This??");
-    console.log(confirm);
-    if (confirm) {
+const Delete = (name, id) => {
+    // const dataStr = JSON.parse(data);
+    DeleteConfirm.innerText = `Are you sure you want to delete ${name} ?`;
+    console.log(name, id);
+    DeleteBtn.addEventListener('click', function () {
         DeleteRequest(id);
-    }
-    else {
-        console.log("abort deletion....");
-    }
+    });
 };
+//function to edit the database;
+// let UpdatedRecipe: Recipe;
+const EditModal = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const preRes = yield fetch(`http://localhost:3000/recipes/${id}`);
+    const preData = yield preRes.json();
+    console.log("pre Data", preData);
+    Uname.value = yield preData.name;
+    Utype.value = yield preData.type;
+    Udesc.value = yield preData.desc;
+    Uid = yield preData.id;
+    console.log(Uname.value);
+});
+const UpdateData = () => __awaiter(void 0, void 0, void 0, function* () {
+    const UpdatedRecipe = {
+        id: Uid,
+        name: Uname.value,
+        type: Utype.value,
+        desc: Udesc.value,
+    };
+    console.log("This is updatedata Parametere...", UpdateData);
+    try {
+        const currentResponse = yield fetch(`http://localhost:3000/recipes/${UpdatedRecipe.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": 'application/json',
+            },
+            body: JSON.stringify(UpdatedRecipe),
+        });
+        const UpdateResponse = yield currentResponse.json();
+        console.log("Updated Response....", UpdateResponse);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+Uform.addEventListener('submit', () => {
+    // e.preventDefault();
+    UpdateData();
+});
--- JS-TS/script.ts
+++ JS-TS/script.ts
@@ -1,14 +1,28 @@
+
 //getting form and form values
 const AddForm = document.querySelector('.form') as HTMLFormElement;
-const id = document.querySelector('#id') as HTMLInputElement;
 const RecipeName = document.querySelector("#name") as HTMLInputElement;
 const type = document.querySelector("#type") as HTMLInputElement;
 const desc = document.querySelector("#desc") as HTMLInputElement;
 
 const ul = document.querySelector('.ul') as HTMLUListElement;
-//Description modal 
+//Recipe View modal 
 const heading = document.querySelector('#RecipeName') as HTMLHeadingElement;
 const description = document.querySelector("#recipeDescription") as HTMLParagraphElement;
+const Recipetype = document.querySelector("#recipeType") as HTMLParagraphElement;
+
+//Delete modal
+const DeleteName = document.querySelector("#DeleteName") as HTMLHeadingElement;
+const DeleteConfirm = document.querySelector("#DeleteConfirm") as HTMLParagraphElement;
+const DeleteBtn = document.querySelector("#Delete")!;
+
+
+//update form value:
+const Uform = document.querySelector('.update-form') as HTMLFormElement;
+const Uname = document.querySelector("#Uname") as HTMLInputElement;
+const Utype = document.querySelector("#Utype") as HTMLInputElement;
+const Udesc = document.querySelector("#Udesc") as HTMLInputElement;
+let Uid:number;
 
 interface Recipe {
     id: number,
@@ -23,9 +37,7 @@ const getData = async () => {
         const res = await fetch('http://localhost:3000/recipes');
         const data = await res.json();
         data.map((item: Recipe) => {
-            console.log(item);
             RenderList(item);
-            // view(item.id);
         })
     } catch (err) {
         console.log(err);
@@ -44,26 +56,20 @@ const RenderList = (listData: Recipe) => {
 
     //this will create the right side span which will have the view update delete button;
     const spanRight = document.createElement('span');
-    spanRight.innerHTML = `<i onclick="viewRecipe(${listData.id})" data-bs-target="#descriptionModal" data-bs-toggle="modal" class="bi bi-eye btn btn-primary"></i> 
-    <i class="bi bi-pencil btn btn-warning"></i> 
-    <i onclick="Delete(${listData.id})" class="bi bi-trash3 btn btn-danger"></i>`
+    spanRight.innerHTML = `
+    <i onclick="viewRecipe(${listData.id})" data-bs-target="#descriptionModal" data-bs-toggle="modal" class="bi bi-eye btn btn-primary"></i> 
+    <i onclick="EditModal('${listData.id}')" data-bs-target="#UpdateModal" data-bs-toggle="modal" class="bi bi-pencil btn btn-warning"></i> 
+    <i data-bs-target="#DeleteModal" data-bs-toggle="modal" onclick="Delete('${listData.name}', ${listData.id})" class="bi bi-trash3 btn btn-danger"></i>`
     li.append(spanLeft, spanRight);
     ul.append(li);
 }
 
-//this function will trigger view modal to view the recipe info:
-const viewRecipe = async (id: (number | string)) => {
-    const res = await fetch(`http://localhost:3000/recipes/${id}`);
-    const recipeData = await res.json();
-    heading.innerText = await recipeData.name;
-    description.innerText = await recipeData.desc;
-}
 
+//submit data on form submit
 AddForm.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-
     const Recipe: Recipe = {
-        id: id.valueAsNumber,
+        id: Date.now(),
         name: RecipeName.value,
         type: type.value,
         desc: desc.value
@@ -88,8 +94,18 @@ const postData = async (data: Recipe) => {
     }
 }
 
+
+//this function will trigger view modal to view the recipe info:
+const viewRecipe = async (id: (number | string)) => {
+    const res = await fetch(`http://localhost:3000/recipes/${id}`);
+    const recipeData = await res.json();
+    heading.innerText = await recipeData.name;
+    description.innerText = await recipeData.desc;
+    Recipetype.innerText = await recipeData.type;
+}
+
 //function to send delete request data from database:
-const DeleteRequest = async (id: number) => {
+const DeleteRequest = async (id: any) => {
     fetch(`http://localhost:3000/recipes/${id}`, {
         method: "DELETE",
     });
@@ -97,12 +113,56 @@ const DeleteRequest = async (id: number) => {
 };
 
 //function to delete the item from database;
-const Delete = (id:number) => {
-    const confirm = window.confirm("Are you sure you want to delete This??");
-    console.log(confirm);
-    if(confirm){
-        DeleteRequest(id);
-    }else{
-        console.log("abort deletion....");
+const Delete = (name: string, id: number) => {
+    // const dataStr = JSON.parse(data);
+    DeleteConfirm.innerText = `Are you sure you want to delete ${name} ?`;
+    console.log(name, id);
+    DeleteBtn.addEventListener('click', function () {
+        DeleteRequest(id)
+    }
+    );
+}
+
+
+//function to edit the database;
+// let UpdatedRecipe: Recipe;
+const EditModal = async (id: number) => {
+    const preRes = await fetch(`http://localhost:3000/recipes/${id}`)
+    const preData = await preRes.json();
+    console.log("pre Data",preData);
+    
+    Uname.value =  await preData.name;
+    Utype.value =  await preData.type;
+    Udesc.value =  await preData.desc;
+    Uid = await preData.id;
+    console.log(Uname.value);
+
+
+}
+
+const UpdateData = async () => {
+    const UpdatedRecipe: Recipe ={
+        id: Uid,
+        name: Uname.value,
+        type: Utype.value,
+        desc: Udesc.value,
+    }
+    console.log("This is updatedata Parametere...",UpdateData);
+    try{
+        const currentResponse = await fetch(`http://localhost:3000/recipes/${UpdatedRecipe.id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-type": 'application/json',
+            },
+            body: JSON.stringify(UpdatedRecipe),
+        });
+        const UpdateResponse = await currentResponse.json();
+        console.log("Updated Response....",UpdateResponse);
+    }catch(err){
+        console.log(err);
     }
 }
+Uform.addEventListener('submit',() => {
+    // e.preventDefault();
+    UpdateData();
+});
